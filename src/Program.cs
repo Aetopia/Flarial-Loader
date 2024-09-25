69bb5e0917af41c5db9431fab16f6cd30dd894ba
Aetopia <41850963+Aetopia@users.noreply.github.com>
2024-09-25 09:56:45 +0530

    Format code.

--- src/Program.cs
+++ src/Program.cs
@@ -14,20 +14,10 @@ static class Program
         using Mutex mutex = new(true, "622D66FB-75AD-47CF-963B-A2C499E9DAF0", out var createdNew); if (!createdNew) return;
         Directory.SetCurrentDirectory(AppDomain.CurrentDomain.BaseDirectory);
 
-     ((NameValueCollection)ConfigurationManager.GetSection("System.Windows.Forms.ApplicationConfigurationSection"))["DpiAwareness"] = "PerMonitorV2";
+        ((NameValueCollection)ConfigurationManager.GetSection("System.Windows.Forms.ApplicationConfigurationSection"))["DpiAwareness"] = "PerMonitorV2";
         Application.EnableVisualStyles();
         Application.SetCompatibleTextRenderingDefault(false);
         Application.SetUnhandledExceptionMode(UnhandledExceptionMode.ThrowException);
-      //  Application.ThreadException += (sender, e) =>
-      //  {
-     //       var exception = e.Exception;
-     //       while (exception.InnerException != null) exception = exception.InnerException;
-     //       Unmanaged.ShellMessageBox(lpcText: exception.Message);
-    //        Environment.Exit(0);
-    //    };
-       Application.Run(new MainForm());
-
-        //content = Flarial.Get("launcher/versions.json");
-        //  Console.WriteLine(content.Update);
+        Application.Run(new MainForm());
     }
 }
